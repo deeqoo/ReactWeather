b32c2a11ca9977e48b31c6007b7640d9ee68fd7e
mhd-ali <deeqoo@deeq-comp.Home>
2016-10-13 15:26:23 +0100

    fixed typo in server.js

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var express = require("express");
 var app = express();
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3000;
+
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
+  if(req.headers['x-forwarded-proto'] === 'https'){
     next();
   }else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
 
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, function(){
   console.log("Express server is up on port " + PORT);
