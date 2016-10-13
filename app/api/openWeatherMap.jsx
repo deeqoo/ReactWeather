58049f427c35f85798372c5bf4637c31db35f33d
MHD Ali <mdeeq.ali@gmail.com>
2016-10-13 18:29:31 +0100

    update axios call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -14,7 +14,7 @@ module.exports = {
         return res.data.main.temp;
       }
     }, function(err){
-      throw new Error(err.data.message);
+      throw new Error(error.response.data.message);
     })
 
   }
