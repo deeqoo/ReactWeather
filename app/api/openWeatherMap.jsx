a5904ef806737e921890e9636800d1646acd2c3e
mhd-ali <deeqoo@deeq-comp.Home>
2016-10-13 18:26:25 +0100

    fix axios api call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -14,7 +14,7 @@ module.exports = {
         return res.data.main.temp;
       }
     }, function(err){
-      throw new Error(error.data.message);
+      throw new Error(err.data.message);
     })
 
   }
