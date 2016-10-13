e43ab1430dd62d96ce86809f0b69c6084d9cc669
mhd-ali <deeqoo@deeq-comp.Home>
2016-10-13 15:51:06 +0100

    remove app.use feat from server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'https'){
-    res.redirect('http://' + req.hostname + req.url);
-  }else {
-    next();
-  }
-});
+// app.use(function(req, res, next){
+//   if(req.headers['x-forwarded-proto'] === 'https'){
+//     res.redirect('http://' + req.hostname + req.url);
+//   }else {
+//     next();
+//   }
+// });
 
 app.listen(PORT, function(){
   console.log("Express server is up on port " + PORT);
