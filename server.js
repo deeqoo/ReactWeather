e8ab147291ffe15341c99e68f4930adc788024e2
mhd-ali <deeqoo@deeq-comp.Home>
2016-10-13 15:33:19 +0100

    trace bug

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'https'){
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   }else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
-
-
 
 app.listen(PORT, function(){
   console.log("Express server is up on port " + PORT);
