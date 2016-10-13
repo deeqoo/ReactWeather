7ac74919562a58c6d41ed93075d21980006caf14
MHD Ali <mdeeq.ali@gmail.com>
2016-10-13 18:47:55 +0100

    update axios error call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,12 +8,14 @@ module.exports = {
     var requestURL = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestURL).then(function(res){
+      debugger;
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       }else {
         return res.data.main.temp;
       }
     }, function(err){
+      debugger;
       throw new Error(error.response.data.message);
     });
 
