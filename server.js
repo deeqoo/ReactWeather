404e16ce307508e841df9dffb3424a0401089362
mhd-ali <deeqoo@deeq-comp.Home>
2016-10-13 16:09:33 +0100

    fixing error server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var app = express();
+
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(function(req, res, next){
-//   if(req.headers['x-forwarded-proto'] === 'https'){
-//     res.redirect('http://' + req.hostname + req.url);
-//   }else {
-//     next();
-//   }
-// });
+app.use(function(req, res, next){
+  if(req.headers['X-Forwarded-Proto'] === 'http'){
+    next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.listen(PORT, function(){
   console.log("Express server is up on port " + PORT);
