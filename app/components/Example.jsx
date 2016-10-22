cde0574db26b42c7f7dd231a54b3cb09c72dc974
MHD Ali <mdeeq.ali@gmail.com>
2016-10-22 15:31:15 +0100

    Apply style to main and add content to example page.

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,19 @@
 var React = require("react");
+var {Link} = require('react-router');
 
 var Example = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are few examples location to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=London">London, UK</Link>
+        </li>
+        <li>
+          <Link to="/?locaiton=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
 
   );
