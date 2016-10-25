1a9cd98e2ad6bf7c8ec35cb39acc870f381d8686
MHD Ali <mdeeq.ali@gmail.com>
2016-10-25 11:12:35 +0100

    Add styles to weather page

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="location" placeholder="Ennter city name" />
-          <button>Get Weather </button>
+          <button className="button expanded hollow">Get Weather </button>
         </form>
       </div>
     );
