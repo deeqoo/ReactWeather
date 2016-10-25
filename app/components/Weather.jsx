0b62e4de657d949a07de093d321dff03c0c03e34
MHD Ali <mdeeq.ali@gmail.com>
2016-10-25 19:53:06 +0100

    Add error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require("react");
 var WeatherForm = require("WeatherForm");
 var WeatherMessage = require("WeatherMessage");
 var OpenWeatherMap = require('OpenWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
 
@@ -13,7 +14,11 @@ getInitialState: function(){
 
 handleSearch: function(location){
   var that = this;
-  this.setState({isLoading: true });
+
+  this.setState({
+    isLoading: true,
+    errorMessage: undefined
+   });
 
   OpenWeatherMap.getTemp(location).then(function(temp){
     that.setState({
@@ -21,26 +26,38 @@ handleSearch: function(location){
       temp: temp,
       isLoading: false
     });
-  }, function(errorMessage){
-      alert(errorMessage);
-      that.setState({isLoading: false});
+  }, function(e){
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
   });
 },
   render: function(){
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, errorMessage,location} = this.state;
 
     function renderMessage(){
       if (isLoading) {
-        return <h3 className="text-center">Fetching weather....</h3>;
+        return <h4 className="text-center">Fetching weather....</h4>;
       }else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>
       }
+    }
+
+    function renderError (){
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        );
+      }
+
     }
      return (
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
 
     );
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       Example: "app/components/Example.jsx",
       WeatherForm: "app/components/WeatherForm.jsx",
       WeatherMessage: "app/components/WeatherMessage.jsx",
-      OpenWeatherMap: "app/api/openWeatherMap.jsx"
+      OpenWeatherMap: "app/api/openWeatherMap.jsx",
+      ErrorModal: "app/components/ErrorModal.jsx"
     },
     extensions: ['', '.js','.jsx']
   },
